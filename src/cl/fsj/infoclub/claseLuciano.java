f0cc35292445612f41ce53ab9d2e352319ab3645
Luciano Cesc <cisternas.luciano@gmail.com>
2022-08-02 22:23:58 -0400

    Ejercicio 2

---
+++ src/cl/fsj/infoclub/claseLuciano.java
@@ -0,0 +1,27 @@
+package cl.fsj.infoclub;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class claseLuciano {
+
+	public static void main(String[] args) {
+		Set<String> invitados = new TreeSet <>();
+		invitados.add("Daniel");
+		invitados.add("Paola");
+		invitados.add("Facundo");
+		invitados.add("Pedro");
+		invitados.add("Jacinta");
+		invitados.add("Florencia");
+		invitados.add("Juan Pablo");
+		
+		Set<String> posiblesInvitados =new HashSet <>(Arrays.asList("Jorge","Francisco","Marcos"));
+		
+		Set<String> listadoFinal = new TreeSet <>(invitados);
+		listadoFinal.addAll(posiblesInvitados);
+		System.out.println(listadoFinal);
+	}
+
+}
