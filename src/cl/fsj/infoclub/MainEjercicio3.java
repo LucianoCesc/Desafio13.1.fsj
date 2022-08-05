3febd5559dd61a5285b729b155c7d1bb961342b1
Silvia2015 <silvitacabellocatapablo@gmail.com>
2022-08-05 18:17:10 -0400

    MainEjercicio3

---
+++ src/cl/fsj/infoclub/MainEjercicio3.java
@@ -0,0 +1,29 @@
+package cl.fsj.infoclub;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MainEjercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Map<String,Integer>Golocinas=new TreeMap<String,Integer>();
+		
+		Golocinas.put("Chocman", 100);
+		Golocinas.put("Trululu", 100);
+		Golocinas.put("Centella", 100);
+		Golocinas.put("Kilate", 50);
+		Golocinas.put("Miti-miti", 50);
+		Golocinas.put("Traga-traga", 150);
+		Golocinas.put("Tableton",5);
+		
+	
+		
+		
+		
+		
+		
+	}
+
+}
