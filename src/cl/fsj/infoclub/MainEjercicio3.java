2f2054f5c106826539aeef0d5553936bbf8d3c87
Edgamer63 <edgamer63@gmail.com>
2022-08-05 18:40:51 -0400

    Ejercicio 4 terminado con ayuda de mis compañeros

--- src/cl/fsj/infoclub/MainEjercicio3.java
+++ src/cl/fsj/infoclub/MainEjercicio3.java
@@ -8,22 +8,21 @@ public class MainEjercicio3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Map<String,Integer>Golocinas=new TreeMap<String,Integer>();
-		Map<String,Integer>Golocinas_Baratas = new TreeMap<String,Integer>();
-		Golocinas.put("Chocman", 100);
-		Golocinas.put("Trululu", 100);
-		Golocinas.put("Centella", 100);
-		Golocinas.put("Kilate", 50);
-		Golocinas.put("Miti-miti", 30);
-		Golocinas.put("Traga-traga", 150);
-		Golocinas.put("Tableton",5);
+		Map<String,Integer>Golosinas=new TreeMap<String,Integer>();
+		Map<String,Integer>Golosinas_Baratas = new TreeMap<String,Integer>();
+		Golosinas.put("Chocman", 100);
+		Golosinas.put("Trululu", 100);
+		Golosinas.put("Centella", 100);
+		Golosinas.put("Kilate", 50);
+		Golosinas.put("Miti-miti", 30);
+		Golosinas.put("Traga-traga", 150);
+		Golosinas.put("Tableton",5);
 		
 		
-		for (Map.Entry<String,Integer> entry : Golocinas.entrySet()) {
-            //System.out.println("Key = " + entry.getKey() +", Value = " + entry.getValue());
-			
+		for (Map.Entry<String,Integer> entry : Golosinas.entrySet()) {
+            
 			if (entry.getValue()<100) {
-				Golocinas_Baratas.put(entry.getKey(), entry.getValue());
+				Golosinas_Baratas.put(entry.getKey(), entry.getValue());
 			}
 		}
 		
---
+++ src/cl/fsj/infoclub/MainEjercicio4.java
@@ -0,0 +1,22 @@
+package cl.fsj.infoclub;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MainEjercicio4 {
+
+	public static void main(String[] args) {
+		Queue juegos = new LinkedList<>();
+		
+		juegos.add("Tombo");
+		juegos.add("Congelado");
+		juegos.add("Quemaditas");
+		juegos.add("Cachipun");
+		juegos.add("Pillarse");
+		
+		System.out.println("Cantidad de Juegos: "+juegos.size());
+		
+		
+	}
+
+}
