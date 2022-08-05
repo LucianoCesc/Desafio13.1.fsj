0a64a672b5d60ff6ba94479f4e32166702ae408d
Edgamer63 <edgamer63@gmail.com>
2022-08-05 18:26:56 -0400

    Ejercicio 3, con ayuda de Silvia

--- src/cl/fsj/infoclub/MainEjercicio3.java
+++ src/cl/fsj/infoclub/MainEjercicio3.java
@@ -9,7 +9,7 @@ public class MainEjercicio3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Map<String,Integer>Golocinas=new TreeMap<String,Integer>();
-		
+		Map<String,Integer>Golocinas_Baratas = new TreeMap<String,Integer>();
 		Golocinas.put("Chocman", 100);
 		Golocinas.put("Trululu", 100);
 		Golocinas.put("Centella", 100);
@@ -18,9 +18,15 @@ public class MainEjercicio3 {
 		Golocinas.put("Traga-traga", 150);
 		Golocinas.put("Tableton",5);
 		
-	
+		for (Map.Entry<String,Integer> entry : Golocinas.entrySet()) {
+            //System.out.println("Key = " + entry.getKey() +", Value = " + entry.getValue());
+			
+			if (entry.getValue()<100) {
+				Golocinas_Baratas.put(entry.getKey(), entry.getValue());
+			}
+		}
 		
-		
+		System.out.println(Golocinas_Baratas);
 		
 		
 		
